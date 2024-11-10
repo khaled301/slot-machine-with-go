30553aa67daa4ccffe334a915c20d3737b594bd3
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-11-10 06:56:54 +0600

    add function to get bet from user

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 // implements formatted I/O
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func getName() string {
 	name := ""
@@ -20,8 +23,43 @@ func getName() string {
 	return name
 }
 
+func getBet(balance uint) uint {
+	var bet uint
+	var input string
+	for true {
+		fmt.Printf("Enter your bet, or O to quit (balance = $%d): ", balance)
+
+		fmt.Scan(&input)
+		inputBet, err := strconv.ParseUint(input, 10, 64) // converts string to uint64
+		bet = uint(inputBet)
+
+		if err != nil {
+			fmt.Println("Invalid input. Please enter a positive number")
+			continue
+		} else if bet > balance {
+			fmt.Println("You don't have enough balance")
+		} else {
+			break
+		}
+	}
+
+	return bet
+}
+
 // entry point function "main"
 func main() {
-	name := getName()
-	fmt.Println(name)
+	balance := uint(200) // var balance unit = 200
+	getName()
+
+	for balance > 0 {
+		bet := getBet(balance)
+
+		if bet == 0 {
+			break
+		}
+
+		balance -= bet
+	}
+
+	fmt.Printf("You left with, $%d.\n", balance)
 }
